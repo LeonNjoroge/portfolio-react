10dd10aa1ad18a1e7a38c962c94c6dc74bb117e4
LeonNjoroge <leonmuraguri@gmail.com>
2025-01-31 02:11:47 +0300

    Update API

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -26,7 +26,7 @@ export const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setButtonText("Sending...");
-    let response = await fetch("../api/contact.js", {
+    let response = await fetch("../api/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
